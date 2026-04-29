716850c85a116b2f8d7aa2da739a66b62b6e952a
Keith Smiley <keithbsmiley@gmail.com>
2026-04-29 12:25:49 -0700

    Add initial packages

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "bazel-spm-examples",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+        .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-collections", from: "1.1.0"),
+        .package(url: "https://github.com/apple/swift-crypto", from: "3.7.0"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.6.0"),
+        .package(url: "https://github.com/apple/swift-nio", from: "2.70.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", from: "601.0.0"),
+
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.15.0"),
+        .package(url: "https://github.com/pointfreeco/swift-perception", from: "1.3.0"),
+
+        .package(url: "https://github.com/Mitek-Systems/MiSnap-iOS", from: "5.9.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.7.0"),
+    ]
+)
