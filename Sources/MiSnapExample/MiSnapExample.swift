dbf6e896387b15ea03e1ad348d71622f494557d6
Keith Smiley <keithbsmiley@gmail.com>
2026-05-06 10:33:03 -0700

    Add more MiSnap imports
    This shows that xcframeworks requires deps for explicit modules

--- Sources/MiSnapExample/MiSnapExample.swift
+++ Sources/MiSnapExample/MiSnapExample.swift
@@ -1,5 +1,14 @@
 import Foundation
+import IDLiveFaceIAD
+import MiSnap
 import MiSnapCore
+import MiSnapFacialCapture
+import MiSnapFacialCaptureUX
+import MiSnapNFC
+import MiSnapNFCUX
+import MiSnapUX
+import MiSnapVoiceCapture
+import MiSnapVoiceCaptureUX
 
 public enum MiSnapExample {
     public static func sha256(_ data: Data) -> String? {
