1f0aeb5680f4378831ca0e12f9e753e1b96ce2b4
Keith Smiley <keithbsmiley@gmail.com>
2026-06-09 16:30:26 -0700

    Add flex example
    This one is interesting since it has deprecation warnings when producing
    the PCM, which get upgraded to errors

---
+++ Sources/FLEXExample/FLEXExample.swift
@@ -0,0 +1,15 @@
+import FLEX
+
+public enum FLEXExample {
+    public static func showExplorer() {
+        FLEXManager.shared.showExplorer()
+    }
+
+    public static func hideExplorer() {
+        FLEXManager.shared.hideExplorer()
+    }
+
+    public static func toggleExplorer() {
+        FLEXManager.shared.toggleExplorer()
+    }
+}
